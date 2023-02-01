d958465d89a9f6bb55d224551ffa0c20e3df72e4
Vi Ngoc Hoang <hoangvn@devcamp.edu.vn>
2023-02-01 15:30:02 +0700

    [Udp] change api url

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import SwaggerUI from 'swagger-ui-react';
 import "swagger-ui-react/swagger-ui.css";
-import 'dotenv';
-const url = process.env.URL_API;
 function App() {
-  console.log("URL === ", url);
   //'https://personal-5zk9yzhc.outsystemscloud.com/SwaggerProxy/rest/ServiceCatalog/230a2680-3474-47aa-a39d-56852edbec7f/swagger.json'
   return (
     <div className="App">
-      <SwaggerUI url={url} />
+      <SwaggerUI url="https://openai.adaptable.app/api" />
     </div>
   );
 }

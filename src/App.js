135fd2651c49937aacffff9a7e866c94e1c2104b
Vi Ngoc Hoang <hoangvn@devcamp.edu.vn>
2023-02-01 14:32:47 +0700

    [Udtp] change url api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import SwaggerUI from 'swagger-ui-react';
 import "swagger-ui-react/swagger-ui.css";
+const url = import.meta.env.URL_API;
 function App() {
   //'https://personal-5zk9yzhc.outsystemscloud.com/SwaggerProxy/rest/ServiceCatalog/230a2680-3474-47aa-a39d-56852edbec7f/swagger.json'
   return (
     <div className="App">
-      <SwaggerUI url='https://askme-j84d.onrender.com/api' />
+      <SwaggerUI url={url} />
     </div>
   );
 }
